64f6167c14d7c648e93f84b4f2bba7ac7172543a
Alex Xavier <ajxv20@gmail.com>
2020-10-26 16:47:01 -0300

    Quantificação de sexos finalizada

--- src/main.c
+++ src/main.c
@@ -4,17 +4,25 @@ int main (void) {
   char sexo;
   int quantidadeMasculino = 0, quantidadeFeminino  = 0, porcentagemMulheres;
 
-  while (sexo != 10) {
+  do {
     printf("============================================================");
     printf("\n                    DESAFIO AIX SISTEMAS                    ");
     printf("\n============================================================\n\n");
 
-    printf("Digite o sexo dos alunos: ");
+    printf("**Para finalizar digite apenas ENTER**\n\n");
+
+    printf("Digite o sexo dos alunos (M / F): ");
     scanf("%c", &sexo);
     getchar();
 
+    if (sexo == 'M' || sexo == 'm') {
+      quantidadeMasculino += 1;
+    } else if (sexo == 'F'  || sexo == 'f') {
+      quantidadeFeminino += 1;
+    }
+
     system("clear");
-  }
+  } while (sexo != 10);
 
   return 0;
 }
